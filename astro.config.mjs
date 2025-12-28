9cba3d810bcc1237f84520631b80fe26459a678f
rojarolando <rojarolando@gmail.com>
2025-12-28 17:19:13 +1300

    Fix: static build for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,17 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import edgeone from '@edgeone/astro';
 
 // https://astro.build/config
 export default defineConfig({
-  /*
-  Currently it is deployed on Tencent Cloud EdgeOne, on an Asia-based Pages platform (the author is also based in Asia). 
-  If users require, they can switch to Vercel or another Pages platform themselves.
-  */
-  adapter: edgeone(),
+  site: 'https://rolandoroja.github.io',
+  output: 'static',
+
   integrations: [tailwind()],
-  server: {
-    port: 4321,
-    host: true
-  },
+
   devToolbar: {
     enabled: false
   },
+
   vite: {
     server: {
       headers: {
